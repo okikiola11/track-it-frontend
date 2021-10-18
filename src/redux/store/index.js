83a46b1ebdd6a1a087a3d36809f6275485280479
okikiola11 <okikiola.apelehin@gmail.com>
2021-10-18 23:38:38 +0000

    Add the usersReducer to the store for state management

---
+++ src/redux/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, combineReducers } from "redux";
+import userReducer from "../reducers/user";
+
+const rootReducer = combineReducers({
+  userStore: userReducer,
+});
+const store = createStore(rootReducer);
+
+export default store;
